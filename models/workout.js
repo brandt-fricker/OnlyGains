cbf33b50f5bd6d5cce5c47984a48ea1ea321b035
Brandt <frickerbrandt@gmail.com>
2021-01-09 11:08:29 -0700

    models added

---
+++ models/workout.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now(),
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: {
+        type: Number,
+        weight: {
+          type: Number,
+          default: 0,
+        },
+        reps: {
+          type: Number,
+          default: 0,
+        },
+        sets: {
+          type: Number,
+          default: 0,
+        },
+        distance: {
+          type: Number,
+          default: 0,
+        },
+      },
+    },
+  ],
+});
+
+const workout = mongoose.model("workout", WorkoutSchema);
+
+module.exports = workout;
